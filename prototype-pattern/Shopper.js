df9f692f923c713557162963de18a9ea45f981a8
Diego Mais <diegomais.dev@gmail.com>
2020-05-31 18:03:51 -0300

    The Prototype pattern In the last commit we took a look at how we might spend a lot of time creating and setting up objects that are essentially the same. Now, we fix those problems by implementing a prototype. We copy everything from line one to line seven. So we copy the setup of the shopper Alex and then we create a new file called scout_prototype.js. We paste this information into the scout_prototype.js file. So in the scout prototype, we modify the code to create a new shopper called a scout and we will add all of these items to the scout's list. We'll also remove the scout's name because we're going to use this instance of this object to create multiple scouts. So the last thing that we need to do is export our scout object. So now we have an object that we set up that has a camping knife, tent, backpack and map already in their shopping list. We go back to the index.js and use this item to improve our code. So instead of importing a shopper, we're going to import the scout prototype. And now instead of creating Alex using the shopper, what we're going to do is use the scout prototype. And by using the scout prototype's clone method, we create an exact clone of the scout prototype in its current state. That means that Alex already has all of these items on their shopping list. And we can simply add a slingshot to him shopping list. Now we have to give this instance a name, so we add alex.name equals "Alex Banks" and we want to do the same thing for Eve, so we copy our scout_prototype.clone call and set Eve using that call too. So, the instance of Eve will again be the scout prototype at it's current state, so Eve will automatically have the same shopping list and the name "Eve Porcello". So we've improved our code, we've eliminated the redundancy. If we had very complicated objects to set up this could save us a lot of time. Now, we go to our shopper.js file and add our clone method so that we can implement the prototype pattern. So the first thing we need to do is to copy the object structure. So we copy all of the prototype methods, the getters and setters for name, the getter for the shopping list and the add item to list, simply by grabbing the current prototype for the current instance by writing var proto = object.getPrototypeOf(this). And this will actually give us the prototype of the current shopper object which is our name, getters and setters for name, shopping list and add item to list. So using the Object.create we create a new object on the fly and add the current prototype of the current instance to that object. Now that gives us the methods of the current object but we also want to clone any current values. So we want to make sure the current shopping list and the current name is also cloned by creating copies of those values. So our cloned object will have a ._name of this._name. So whatever this object is currently named will be our cloned name. Our clone shopping list is also going to equal an array. And need to set the shopping list to a new array to square brackets. Now using the spread operator we can copy all of the values from this _shopping list into our cloned shopping list. And the last thing that we need to do is to return the cloned object. So there we go. Adding that clone method to the shopper class will allow us to use the prototype pattern when we create new shoppers off of the scout prototype. Running on terminal the command node index.js and we can see that we have two instances of shopper objects that both were created using the scout prototype. So they both have camping knife, tent, backpack and map. All of that set up was obtained through the scout prototype. So that's where all of the items were added and now we can create as many shoppers that are going to go on this camping trip as we like using the scout prototype clone method.

--- prototype-pattern/Shopper.js
+++ prototype-pattern/Shopper.js
@@ -19,6 +19,16 @@ class Shopper {
   addItemToList(item) {
     this._shoppingList.push(item);
   }
+
+  clone() {
+    var proto = Object.getPrototypeOf(this);
+    var cloned = Object.create(proto);
+
+    cloned._name = this._name;
+    cloned._shoppingList = [...this._shoppingList];
+
+    return cloned;
+  }
 }
 
 module.exports = Shopper;
--- prototype-pattern/index.js
+++ prototype-pattern/index.js
@@ -1,17 +1,11 @@
-var Shopper = require("./Shopper");
+var scout_prototype = require("./scout_prototype");
 
-var alex = new Shopper("Alex Banks");
-alex.addItemToList("camping knife");
-alex.addItemToList("tent");
-alex.addItemToList("backpack");
-alex.addItemToList("map");
+var alex = scout_prototype.clone();
+alex.name = "Alex Banks";
 alex.addItemToList("slingshot");
 
-var eve = new Shopper("Eve Porcello");
-eve.addItemToList("camping knife");
-eve.addItemToList("tent");
-eve.addItemToList("backpack");
-eve.addItemToList("map");
+var eve = scout_prototype.clone();
+eve.name = "Eve Porcello";
 eve.addItemToList("reading light");
 
 console.log(`${alex.name}: ${alex.shoppingList}`);
