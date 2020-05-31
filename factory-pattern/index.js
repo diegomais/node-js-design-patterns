7d6c759077122e0b039cfb01efd215f538c9134a
Diego Mais <diegomais.dev@gmail.com>
2020-05-31 18:36:35 -0300

    The Factory pattern Instead of using classes directly within our client application, we use a factory method instead, so we can import something called the user factory. And instead of using the instances to create instances of my users, we actually let the factory decide which type of user to create. So, by sending a few arguments to this user factory method, we get it to return the right type of instance. So we add a new file and this is our userFactory.js. And now within our user factory, this is where we actually use all of the classes required to create objects. So we need employee class and we also need shopper class, so we add a function that we can use to create object instances for us. Now this function's going to take in some arguments, specifically the name of the user to create, how much money they have, what type of user we're supposed to be creating, and optionally an employer, if they are an employee. And we use this information to decide which type of object to return. So if type is equal to employee, then we'll return a new employee, with the name, money, and employer that was passed to our method. Otherwise, we return a new shopper with the name and the money that was passed to this method. Now we have a factory method that can create employees or shoppers. Running our index.js we can see that we've created Alex Banks with some money, but Alex Banks is just a shopper, so he's not employed. And we also have Eve Porcello and Eve has some money as well and she is employed. So the benefit of using the factory method is it encapsulates everything that we need to create users within one method. Later on, if we expand, we can actually create different types of users from this same method.

--- factory-pattern/index.js
+++ factory-pattern/index.js
@@ -1,8 +1,9 @@
-var Shopper = require("./Shopper");
-var Employee = require("./Employee");
+var userFactory = require("./userFactory");
 
-var alex = new Shopper("Alex Banks", 100);
-var eve = new Employee("Eve Porcello", 100, "This and That");
+var alex = userFactory("Alex Banks", 100);
+var eve = userFactory("Eve Porcello", 100, "employee", "This and That");
+
+eve.payDay(100);
 
 console.log(alex.toString());
 console.log(eve.toString());
