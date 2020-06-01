5c3cff69f4e2c81822c9dfcb7d3c90de0fc0d9e0
Diego Mais <diegomais.dev@gmail.com>
2020-06-01 12:33:08 -0300

    The Chain of Responsibility pattern First, in the index.js, we need to send the entire inventory catalog to the store, not just the items on the floor. In the Store's constructor, as opposed to just setting up an inventory for the store, we are going to set up several storage locations using our new objects. Now the thing about getting inventory items from a local store or a warehouse, is that might take some time. We add another argument, which is the time and days it might take your inventory item to reach our store if it's found in another store. And then we set the next place to look for items. And we modify the find method to instead of returning an item from this store's inventory, it will try to find that item in storage. So we will return this.storage.find item name. The first request will be piped through several storage objects until the inventory item is found. We add a new file called storage.js. And in our storage file we create a new class, the Storage's constructor take in the name of the storage location, the inventory for that location with a an array as default value and also a delivery time with default to zero. And we set up the next variable with null, because we don't have a next value. Add a setNext method that expect another storage class. Add lookInLocalInventory method to find the item by locating the index of the item, if we have one, by taking a new array of just the item name. And then I can use the index of to just grab the one that matches our item name. If we don't have the item the method will return undefined. We add a find method to first look in the local inventory. So if this storage object has a next object, then we will look there. But if this storage object doesn't have a next item and we haven't found the item tself, what we want to do is return a string saying that we don't carry this item. So all of this is possible because the store itself implements storage, and the storage object implements the chain of responsibility design pattern.

--- chain-of-responsibility-pattern/Store.js
+++ chain-of-responsibility-pattern/Store.js
@@ -1,12 +1,23 @@
+var Storage = require("./Storage");
+
 class Store {
   constructor(name, inventory = []) {
     this.name = name;
-    this.inventory = inventory;
+
+    var floor = new Storage("store floor", inventory.floor);
+    var backroom = new Storage("store backroom", inventory.backroom);
+    var localStore = new Storage("nearby store", inventory.localStore, 1);
+    var warehouse = new Storage("warehouse", inventory.warehouse, 5);
+
+    floor.setNext(backroom);
+    backroom.setNext(localStore);
+    localStore.setNext(warehouse);
+
+    this.storage = floor;
   }
 
   find(itemName) {
-    var index = this.inventory.map((item) => item.name).indexOf(itemName);
-    return this.inventory[index];
+    return this.storage.find(itemName);
   }
 }
 
--- chain-of-responsibility-pattern/index.js
+++ chain-of-responsibility-pattern/index.js
@@ -1,9 +1,9 @@
 var Store = require("./Store");
 var inventory = require("./inventory");
 
-var skiShop = new Store("Steep and Deep", inventory.floor);
+var skiShop = new Store("Steep and Deep", inventory);
 
-var searchItem = "ski hats";
+var searchItem = "powder skis";
 var results = skiShop.find(searchItem);
 
 console.log(results);
